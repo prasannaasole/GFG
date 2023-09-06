034a8c55659f0d995db1d91bf150f27330a6b4e6
Prasanna Asole <121611874+prasannaasole@users.noreply.github.com>
2023-09-06 22:48:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Mother Vertex/mother-vertex.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+            }
+            Solution obj = new Solution();
+            int ans = obj.findMotherVertex(V, adj);
+            System.out.println(ans);
+       }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+         int array[] = new int[V];
+        int lastNode = 0;
+        
+        for(int i=0; i<V; i++){
+            if(array[i]==0){
+                dfs(adj,i,array);
+                lastNode = i;
+            }
+        }
+        Arrays.fill(array,0);
+        dfs(adj,lastNode,array);
+        for(int x : array){
+            if(x==0) return -1;
+        }
+        return lastNode;
+    }
+    
+    void dfs(ArrayList<ArrayList<Integer>> adj, int k, int[] array){
+        array[k] = 1;
+        for(int x : adj.get(k)){
+            if(array[x]==0){
+                dfs(adj,x,array);
+            }
+        }
+    }
+}
