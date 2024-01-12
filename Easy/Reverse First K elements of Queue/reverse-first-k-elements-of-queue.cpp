d32c86cf347cd42291319c6bc3adfcd91443bc4e
Prasanna Asole <121611874+prasannaasole@users.noreply.github.com>
2024-01-12 19:57:05 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reverse First K elements of Queue/reverse-first-k-elements-of-queue.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        // add code here.
+          stack<int>s;
+         queue<int>ans;
+        while(k>0)
+        {
+            s.push(q.front());
+            q.pop();
+            k--;
+        }
+        while(!s.empty())
+        {
+            ans.push(s.top());
+            s.pop();
+        }
+        while(!q.empty())
+        {
+             ans.push(q.front());
+             q.pop();
+        }
+       
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n, k;
+        cin >> n >> k;
+        queue<int> q;
+        while (n-- > 0) {
+            int a;
+            cin >> a;
+            q.push(a);
+        }
+        Solution ob;
+        queue<int> ans = ob.modifyQueue(q, k);
+        while (!ans.empty()) {
+            int a = ans.front();
+            ans.pop();
+            cout << a << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
