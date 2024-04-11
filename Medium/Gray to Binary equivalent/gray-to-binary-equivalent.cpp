a3432d6e2a52b99ef3959228a5a2c5a99f5cadc2
Prasanna Asole <121611874+prasannaasole@users.noreply.github.com>
2024-04-11 20:38:30 +0530

    Updated Solution - GfG to GitHub

--- Medium/Gray to Binary equivalent/gray-to-binary-equivalent.cpp
+++ Medium/Gray to Binary equivalent/gray-to-binary-equivalent.cpp
@@ -8,34 +8,34 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution{
-    public:
-    // function to convert a given Gray equivalent n to Binary equivalent.
-    int grayToBinary(int n)
-    {
-        
-        // Your code here
-         vector<int> binary;
-        while(n)
-        {
-            binary.push_back(n&1);
-            n=n/2;
-        }
-        reverse(binary.begin(), binary.end());
-        for(int i = 1; i < binary.size(); i++)
-        {
-            binary[i] = binary[i]^binary[i-1];
-        }
-        int counter = binary.size()-1;
-        int ans = 0;
-        for(int i = 0; i < binary.size(); i++)
-        {
-            ans += binary[i] * (pow(2,counter));
-            counter--;
-        }
-        return ans;
-        
-    }
+        class Solution{
+            public:
+            // function to convert a given Gray equivalent n to Binary equivalent.
+            int grayToBinary(int n)
+            {
+                
+                // Your code here
+                 vector<int> binary;
+                while(n)
+                {
+                    binary.push_back(n&1);
+                    n=n/2;
+                }
+                reverse(binary.begin(), binary.end());
+                for(int i = 1; i < binary.size(); i++)
+                {
+                    binary[i] = binary[i]^binary[i-1];
+                }
+                int counter = binary.size()-1;
+                int ans = 0;
+                for(int i = 0; i < binary.size(); i++)
+                {
+                    ans += binary[i] * (pow(2,counter));
+                    counter--;
+                }
+                return ans;
+                
+            }
 };
 
 //{ Driver Code Starts.
