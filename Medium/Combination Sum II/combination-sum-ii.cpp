8bc4518019dd843c270b6ec4f131a77e3b7f1af9
Prasanna Asole <121611874+prasannaasole@users.noreply.github.com>
2024-05-10 16:25:16 +0530

    Updated Solution - GfG to GitHub

--- Medium/Combination Sum II/combination-sum-ii.cpp
+++ Medium/Combination Sum II/combination-sum-ii.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Solution{
 public:
-    void help(vector<int>&arr,int n,int k,int i,int sum,vector<int>&temp,set<vector<int>>&st  ){
+   void help(vector<int>&arr,int n,int k,int i,int sum,vector<int>&temp,set<vector<int>>&st  ){
     if(sum==k){
     st.insert(temp);
     return;
@@ -30,7 +30,8 @@ public:
         set<vector<int>>st;
         help(arr,n,k,0,0,temp,st);
         vector<vector<int>>ans(st.begin(),st.end());
-        return ans;}
+        return ans;
+    }
 };
 
 //{ Driver Code Starts.
